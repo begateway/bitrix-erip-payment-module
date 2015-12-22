f75dab29404b9c750d5504183ae7ea49d6447e3c
dimabresky <375447411180@yandex.ru>
2015-12-22 18:00:39 +0300

    added setup options module

---
+++ devtm.erip/lang/ru/options.php
@@ -0,0 +1,14 @@
+<?
+$MESS["DEVTM_ERIP_DOMAIN_API_DESC"] = "Домен API";
+$MESS["DEVTM_ERIP_SHOP_ID_DESC"] = "ID магазина";
+$MESS["DEVTM_ERIP_SHOP_KEY_DESC"] = "Ключ магазина";
+$MESS["DEVTM_ERIP_NOTIFICATION_URL_DESC"] = "Страница уведомления после оплаты";
+$MESS["DEVTM_ERIP_SERVICE_NUMBER_DESC"] = "Номер сервиса в системе ЕРИП";
+$MESS["DEVTM_ERIP_COMPANY_NAME_DESC"] = "Имя компании в системе ЕРИП";
+$MESS["DEVTM_ERIP_PATH_TO_SERVICE_DESC"] = "Путь к услуге в дереве системы ЕРИП";
+$MESS["DEVTM_ERIP_FOR_PAYER_DESC"] = "Описание сервиса для плательщика";
+$MESS["DEVTM_ERIP_TAB_NAME"] = "Настройка модуля";
+$MESS["DEVTM_ERIP_TAB_DESC"] = "Настройка модуля Расчёт(ЕРИП)";
+$MESS["DEVTM_ERIP_SAVE_BTN_NAME"] = "Сохранить";
+$MESS["DEVTM_ERIP_RESET_BTN_NAME"] = "Сбросить";
+$MESS["DEVTM_ERIP_RESTORE_WARNING"] = "Текущие значения будут удалены.";
---
+++ devtm.erip/options.php
@@ -0,0 +1,78 @@
+<?
+
+use Bitrix\Main\Localization\Loc;
+
+if( ! $USER->isAdmin() || ! \Bitrix\Main\Loader::includeModule("sale") ) return ;
+
+Loc::loadMessages(__FILE__);
+
+global $APPLICATION;
+
+$module_id = "devtm.erip";
+
+$all_options = array(
+					"domain_api" =>Loc::getMessage("DEVTM_ERIP_DOMAIN_API_DESC"),
+					"shop_id" => Loc::getMessage("DEVTM_ERIP_SHOP_ID_DESC"),
+					"shop_key" => Loc::getMessage("DEVTM_ERIP_SHOP_KEY_DESC"),
+					"notification_url" => Loc::getMessage("DEVTM_ERIP_NOTIFICATION_URL_DESC"),
+					"service_number" => Loc::getMessage("DEVTM_ERIP_SERVICE_NUMBER_DESC"),
+					"company_name" => Loc::getMessage("DEVTM_ERIP_COMPANY_NAME_DESC"),
+					"path_to_service" => Loc::getMessage("DEVTM_ERIP_PATH_TO_SERVICE_DESC"),
+					"description_for_payer" => Loc::getMessage("DEVTM_ERIP_FOR_PAYER_DESC"),
+				);
+$tabs = array(
+			array(
+				"DIV" => "edit1",
+				"TAB" => Loc::getMessage("DEVTM_ERIP_TAB_NAME"),
+				"ICON" => "erip-icon",
+				"TITLE" => Loc::getMessage("DEVTM_ERIP_TAB_DESC")
+			),
+		);
+		
+$o_tab = new CAdminTabControl("EripTabControl", $tabs);
+
+if( $REQUEST_METHOD == "POST" && strlen( $save . $reset ) > 0 && check_bitrix_sessid() )
+{
+	if( strlen($reset) > 0 )
+	{
+		foreach( $all_options as $name => $desc )
+		{
+			\Bitrix\Main\Config\Option::delete( $module_id, $name );
+		}
+	}
+	else
+	{
+		foreach( $all_options as $name => $desc )
+		{
+			if( isset( $_REQUEST[$name] ) )
+				\Bitrix\Main\Config\Option::set( $module_id, $name, $_REQUEST[$name] );
+		}
+	}
+	
+	LocalRedirect($APPLICATION->GetCurPage()."?mid=".urlencode($module_id)."&lang=".urlencode(LANGUAGE_ID)."&".$o_tab->ActiveTabParam());
+}
+
+$o_tab->Begin();
+?>
+
+<form method="post" action="<?echo $APPLICATION->GetCurPage()?>?mid=<?=urlencode($module_id)?>&amp;lang=<?echo LANGUAGE_ID?>">
+<?
+$o_tab->BeginNextTab();
+foreach( $all_options as $name => $desc ):
+	$cur_opt_val = Bitrix\Main\Config\Option::get( $module_id, $name );
+?>
+	<tr>
+		<td width="40%">
+			<label for="<?echo htmlspecialcharsbx($name)?>"><?echo $desc?>:</label>
+		</td>
+		<td width="60%">
+			<input type="text" value="<?echo htmlspecialcharsbx($cur_opt_val)?>" name="<?echo htmlspecialcharsbx($name)?>">
+		</td>
+	</tr>
+<?endforeach?>
+<?$o_tab->Buttons();?>
+	<input type="submit" name="save" value="<?= Loc::getMessage("DEVTM_ERIP_SAVE_BTN_NAME")?>" title="<?= Loc::getMessage("DEVTM_ERIP_SAVE_BTN_NAME")?>" class="adm-btn-save">
+	<input type="submit" name="reset" title="<?= Loc::getMessage("DEVTM_ERIP_RESET_BTN_NAME")?>" OnClick="return confirm('<?echo AddSlashes(Loc::getMessage("DEVTM_ERIP_RESTORE_WARNING"))?>')" value="<?= Loc::getMessage("DEVTM_ERIP_RESET_BTN_NAME")?>">
+	<?=bitrix_sessid_post();?>
+<?$o_tab->End();?>
+</form>
