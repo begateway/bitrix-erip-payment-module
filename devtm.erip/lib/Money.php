e667151b670a874699ad0c3a645f7054714af332
avsh <avterus@gmail.com>
2016-02-12 13:48:23 +0300

    limit allowed currency to BYR/BYN

--- devtm.erip/lib/Money.php
+++ devtm.erip/lib/Money.php
@@ -5,30 +5,30 @@ class Money
 {
 	protected $amount;
 	protected $currency;
-	
+
 	public function setAmount($amount)
 	{
 		if(!is_numeric($amount))
 			throw new Exception("Price must be a number");
-		
+
 		$this->amount = $amount;
 	}
-	
+
 	public function getAmount()
 	{
-		return $this->amount;
+		return ($this->currency == "BYR") ? $this->amount : $this->amount * 100;
 	}
-	
+
 	public function setCurrency($currency)
 	{
-		$allowed_currency = array("RUB", "BYR",	"USD", "EUR");
-		
+		$allowed_currency = array("BYR", "BYN");
+
 		if(!in_array($currency,$allowed_currency))
-			throw new Exception("An invalid currency symbol");
-		
+			throw new \Exception("An invalid currency symbol");
+
 		$this->currency = $currency;
 	}
-	
+
 	public function getCurrency()
 	{
 		return $this->currency;
